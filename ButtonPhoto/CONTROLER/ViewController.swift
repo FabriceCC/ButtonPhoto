0109e5b4703dc2de74f172247904a09c3ce4a998
fabrice <fabrice.conchou@laposte.net>
2018-12-13 10:31:29 +0100

    Erreurs sur caméra sur mon iPhone

--- ButtonPhoto/CONTROLER/ViewController.swift
+++ ButtonPhoto/CONTROLER/ViewController.swift
@@ -17,15 +17,55 @@ UINavigationControllerDelegate{
     
     
     @IBAction func BoutonTouch(_ sender: AnyObject) {
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+        let caméra = UIImagePickerController.isSourceTypeAvailable(.camera)
+        let phototèque = UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        
+        switch (caméra, phototèque) {
+        case (true, true):
+            let Choice = UIAlertController(title:nil, message: nil, preferredStyle: .actionSheet)
+            let actionCamera = UIAlertAction(title: "Caméra", style: .default, handler: { _ in self.Selection(pour: .camera)})
+            let actionPhototeque = UIAlertAction(title: "Phototèque", style: .default, handler: { _ in self.Selection(pour: .photoLibrary)})
+            let actionAnnuler = UIAlertAction(title: "Annuler", style: .cancel)
+            Choice.addAction(actionCamera)
+            Choice.addAction(actionPhototeque)
+            Choice.addAction(actionAnnuler)
+            present(Choice, animated: true)
+        case (true, false) :
+            var imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+            
+        case (false, true) :
             var imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = .photoLibrary;
             imagePicker.allowsEditing = true
             self.present(imagePicker, animated: true, completion: nil)
+        default :
+            break
         }
+        //if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+          //  var imagePicker = UIImagePickerController()
+        //imagePicker.delegate = self
+          //  imagePicker.sourceType = .photoLibrary;
+            //imagePicker.allowsEditing = true
+           // self.present(imagePicker, animated: true, completion: nil)
+       // }
         }
-    // test commit
+   
+    func Selection(pour typeSource: UIImagePickerController.SourceType) {
+        let selecteur = UIImagePickerController()
+        selecteur.sourceType = typeSource
+        selecteur.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        present(selecteur, animated: true)
+        
+        
+    }
+    
+    
+    
         override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
